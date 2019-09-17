9e5727f25eba2f741980f01a0c209decd0e05565
mjachowdhury <study.computing2016@gmail.com>
2019-09-17 11:42:20 +0100

    example of protype, can writer function under the main then just get the function signature on top of the main

--- FunctionExample/main.c
+++ FunctionExample/main.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-double cubeReturn(int num){
-    return num * num * num;
-}
+double cubeReturn(double num);
+
 int main()
 {
-    printf("Cube answer is %f : ", cubeReturn(12));
+    printf("Cube answer is %f : ", cubeReturn(12.0));
     return 0;
+}
+
+double cubeReturn(double num){
+    return num * num * num;
 }
