0d2b406b88ac8fbdec096c874d4758cd19ada284
mjachowdhury <study.computing2016@gmail.com>
2019-09-17 13:10:54 +0100

    multiple struct creating example

--- StructExample/main.c
+++ StructExample/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//with struct i can create a data structure.
 struct Phone {
     char phoneName[15];
     char phoneManufacture[20];
@@ -10,7 +11,7 @@ struct Phone {
 int main()
 {
     printf("Struct Exaple!\n");
-    //createing struct
+    //createing struct and i can create multiple struct
     struct Phone phone1;
     phone1.phonePrice = 100.50;
     strcpy(phone1.phoneName, "Samsung");
@@ -21,5 +22,16 @@ int main()
     printf("%s Phone Manufacture \n", phone1.phoneManufacture);
     printf("%f Phone Price \n", phone1.phonePrice);
     printf("%s Phone Color \n", phone1.phoneColor);
+
+    struct Phone phone2;
+    phone2.phonePrice = 200.50;
+    strcpy(phone2.phoneName, "Nokia");
+    strcpy(phone2.phoneManufacture, "Nokia");
+    strcpy(phone2.phoneColor, "Blue");
+
+    printf("%s Phone Name \n", phone2.phoneName);
+    printf("%s Phone Manufacture \n", phone2.phoneManufacture);
+    printf("%f Phone Price \n", phone2.phonePrice);
+    printf("%s Phone Color \n", phone2.phoneColor);
     return 0;
 }
