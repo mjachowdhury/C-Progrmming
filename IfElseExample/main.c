5a98bd6afe6ca65a2118b00f331514e32d55849f
mjachowdhury <study.computing2016@gmail.com>
2019-09-17 12:25:38 +0100

    example of third parameters with else if

--- IfElseExample/main.c
+++ IfElseExample/main.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int maxFinder(int num1, int num2){
+int maxFinder(int num1, int num2, int num3){
     int result;
 
-    if(num1 > num2){
+    if(num1 > num2 && num1 > 3){
         result = num1;
-    } else{
+    } else if(num2 > num1 && num2 > num3){
         result = num2;
+    }
+    else{
+        result = num3;
     }
     //printf("Max number is :",result);
     return result;
@@ -16,13 +19,17 @@ int main()
 {
     int num1;
     int num2;
+    int num3;
 
     printf("Enter first numbers : ");
     scanf("%d", &num1);
 
     printf("Enter Second numbers : ");
     scanf("%d", &num2);
-    printf("%d , is the max number ", maxFinder(num1, num2));
+
+    printf("Enter the third number: ");
+    scanf("%d", &num3);
+    printf("%d , is the max number ", maxFinder(num1, num2, num3));
 
     return 0;
 }
