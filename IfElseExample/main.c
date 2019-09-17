35619d6cfa403383df7c7f343b7d7e2aeb942a81
mjachowdhury <study.computing2016@gmail.com>
2019-09-17 12:18:15 +0100

    with if else find out out max number getting inpout from the user example

--- IfElseExample/main.c
+++ IfElseExample/main.c
@@ -1,18 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int maxFinder(int num1 , int num2){
+int maxFinder(int num1, int num2){
     int result;
+
     if(num1 > num2){
         result = num1;
     } else{
         result = num2;
     }
+    //printf("Max number is :",result);
     return result;
 }
 int main()
 {
-    maxFinder(45,85);
-    printf("Max number is %d : \n");
+    int num1;
+    int num2;
+
+    printf("Enter first numbers : ");
+    scanf("%d", &num1);
+
+    printf("Enter Second numbers : ");
+    scanf("%d", &num2);
+    printf("%d , is the max number ", maxFinder(num1, num2));
+
     return 0;
 }
